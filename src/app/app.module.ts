1ef85317b67d62782fbee3d037e19c19598ef858
victorbasset <victor.basset@yahoo.fr>
2018-01-26 14:52:08 +0100

    Vue de mon profil terminé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -14,7 +13,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -26,7 +24,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
+  name:string;
+  greeting:string;
+  balance:number;
+  eyeColor:string;
+  age:number;
+  company:string;
+  email:string;
+  phone:string;
+  address:string;
+  about:string;
+  favoriteFruit:string;
+
+  ngOnInit() {
+    this.name = "Jessie";
+    this.greeting = "Hello Jessie ! You have 9 unread messages.";
+    this.balance = 2987.20;
+    this.eyeColor = "green";
+    this.age = 23;
+    this.company = "KINGJOUET";
+    this.email = "jessie@toy.fr";
+    this.phone = "066666666666";
+    this.address = "Versaille story";
+    this.about = "Est exercitation reprehenderit elit sit. Laborum duis consequat duis officia enim anim voluptate fugiat minim aute ad esse. Qui mollit nisi qui elit laborum reprehenderit nulla minim commodo. Fugiat id sit ipsum excepteur minim irure veniam.";
+    this.favoriteFruit = "banana";
+  }
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
